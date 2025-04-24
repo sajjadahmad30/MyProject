2981d7c536e8e4e5729a24614d15fff6cb32ffd3
sajjad ahmad <sajjadahmad5578@gmail.com>
2025-04-24 14:02:38 +0500

    middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from "cloudinary";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:  process.env.CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET,
+});
+
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        //upload th file to cloudinary
+        const response =  await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully
+        console.log("file uploaded on cloudinary ", response.url);
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved file as the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
